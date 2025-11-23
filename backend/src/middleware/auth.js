2dbcf8060c0cafc9a5f35f03ba677754eebd2ab0
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-11-23 19:40:29 +0100

    add jwt auth middleware

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+const auth = (req, res, next) => {
+    const header = req.headers.authorization
+    if(!header) {
+        return res.status(401).json({error: "Token not provided."})
+    }
+    const token = header.split(" ")[1]
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch(err) {
+        return res.status(401).json({error: "Invalid token."})
+    }
+}
+
+export default auth
