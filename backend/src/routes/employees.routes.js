ac6881fd4e3d3a131eed99c57394a5b634d4f614
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-11-23 19:44:51 +0100

    secure employees api with jwt middleware

--- backend/src/routes/employees.routes.js
+++ backend/src/routes/employees.routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import prisma from '../config/prisma.js'
+import auth from '../middleware/auth.js'
 
 const router = express.Router()
 
-router.get('/employees', async (req, res) => {
+router.get('/employees', auth, async (req, res) => {
     try {
         const employees = await prisma.employee.findMany()
-        console.log(employees)
         res.json(employees)
     } catch(err) {
         res.status(500)
