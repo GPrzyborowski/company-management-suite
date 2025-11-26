d7f3cee644f503445a1a8cc21c49a885590500e8
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-11-26 05:24:18 +0100

    move login form to external component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,40 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import Navigation from '../components/Navigation'
+import LoginForm from '../components/LoginForm'
 
 function Login() {
+	const [login, setLogin] = useState('')
+	const [password, setPassword] = useState('')
 
-    const [login, setLogin] = useState('')
-    const [password, setPassword] = useState('')
+	const tempEndpoint = 'http://localhost:5000/api/auth/login'
 
-    const tempEndpoint = "http://localhost:5000/api/auth/login"
+	async function submit() {
+		const res = await fetch(tempEndpoint, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ login, password }),
+		})
 
-    async function submit(e) {
-        e.preventDefault()
-        const res = await fetch(tempEndpoint, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({login, password})
-        })
+		const data = await res.json()
 
-        const data = await res.json()
+		if (res.ok) {
+			localStorage.setItem('token', data.jwtToken)
+		} else {
+			console.error(data.error)
+		}
+	}
 
-        if(res.ok) {
-            localStorage.setItem('token', data.jwtToken)
-        } else {
-            console.error(data.error)
-        }
-    }
-
-    return (
-        <> 
-        <form onSubmit={submit}>
-            <label htmlFor="login"></label>
-            <input type="text" id="login" value={login} onChange={(e) => setLogin(e.target.value)}/> <br />
-            <label htmlFor="password"></label>
-            <input type="text" id='password' value={password} onChange={(e) => setPassword(e.target.value)}/> <br />
-        <button type='submit'>Login</button>
-        </form> 
-        </>
-    )
+	return (
+		<>
+			<Navigation />
+			<LoginForm onSubmit={submit} login={login} setLogin={setLogin} password={password} setPassword={setPassword} />
+		</>
+	)
 }
 
 export default Login
