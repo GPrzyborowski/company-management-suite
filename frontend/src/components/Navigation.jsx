f623f08900cda1a72426f32450a275620d9f2c2e
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-11-26 05:26:45 +0100

    improve burger icon styling and add home link to navigation

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -9,14 +9,23 @@ function Navigation() {
 	return (
 		<nav className={classes.nav}>
 			<button className={classes.burger} onClick={() => setOpen(!open)}>
-                <img src={BurgerIcon} alt="burger icon" />
+				<img src={BurgerIcon} alt="burger icon" className={classes['burger-bars']} />
 			</button>
 			<ul className={`${classes.list} ${open ? `${classes.opened}` : `${classes.closed}`}`}>
 				<li className={classes['list-item']}>
-					<Link to="/register" className={classes['list-item-link']}>Register</Link>
+					<Link to="/" className={classes['list-item-link']}>
+						Home
+					</Link>
 				</li>
 				<li className={classes['list-item']}>
-					<Link to="/login" className={classes['list-item-link']}>Login</Link>
+					<Link to="/register" className={classes['list-item-link']}>
+						Register
+					</Link>
+				</li>
+				<li className={classes['list-item']}>
+					<Link to="/login" className={classes['list-item-link']}>
+						Login
+					</Link>
 				</li>
 			</ul>
 		</nav>
