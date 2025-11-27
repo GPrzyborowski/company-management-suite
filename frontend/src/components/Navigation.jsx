384d33ee9a08d1baffc48cd0b4fa6463f91032cf
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-11-27 21:34:17 +0100

    remove dynamic color change for burger icon

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,17 +1,15 @@
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import classes from './Navigation.module.css'
-import BurgerIconBlue from '../assets/menu_blue.svg'
-import BurgerIconWhite from '../assets/menu_white.svg'
+import BurgerIcon from '../assets/burger_icon.svg'
 
 function Navigation() {
 	const [open, setOpen] = useState(false)
-	const burgerIconSource = open ? BurgerIconWhite : BurgerIconBlue
 
 	return (
 		<nav className={classes.nav}>
 			<button className={classes.burger} onClick={() => setOpen(!open)}>
-				<img src={burgerIconSource} alt="burger icon" className={classes['burger-bars']} />
+				<img src={BurgerIcon} alt="burger icon" className={classes['burger-bars']} />
 			</button>
 			<ul className={`${classes.list} ${open ? `${classes.opened}` : `${classes.closed}`}`}>
 				<li className={classes['list-item']}>
