38531e3d1c414fc89e71fb35e738a81d1cd73646
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-11-28 22:41:27 +0100

    setup base dashboard route

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Navigation from "../components/Navigation"
+import DashboardMain from "../components/DashboardMain"
+
+function Dashboard() {
+    return (
+        <>
+            <Navigation />
+            <DashboardMain />
+        </>
+    )
+}
+
+export default Dashboard
