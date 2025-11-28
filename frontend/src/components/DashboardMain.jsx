6d806e4a65f3d306acf7ae1eb8b28427191e27a4
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-11-28 22:42:45 +0100

    create dashboard body component and add background

---
+++ frontend/src/components/DashboardMain.jsx
@@ -0,0 +1,9 @@
+import classes from './DashboardMain.module.css'
+
+function DashboardMain() {
+    return (
+        <main className={classes['dashboard-container']}></main>
+    )
+}
+
+export default DashboardMain
