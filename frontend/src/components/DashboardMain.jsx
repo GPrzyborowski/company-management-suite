73a322435278537bc47b270037feaa23b2f4d038
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-11-30 23:42:51 +0100

    decode jwt token and display login on dashboard

--- frontend/src/components/DashboardMain.jsx
+++ frontend/src/components/DashboardMain.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react'
+import { jwtDecode } from 'jwt-decode'
 import classes from './DashboardMain.module.css'
 
 function DashboardMain() {
+
+    const [login, setLogin] = useState('')
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if(!token) {
+            return
+        }
+        try {
+            const decoded = jwtDecode(token)
+            setLogin(decoded.login)
+        } catch(err) {
+            console.error(err)
+        }
+    }, [])
+
+    
+
     return (
-        <main className={classes['dashboard-container']}></main>
+        <main className={classes['dashboard-container']}>
+            <h1 className={classes['main-header']}>Hello {login}</h1>
+        </main>
     )
 }
 
