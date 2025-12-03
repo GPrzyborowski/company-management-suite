29c1141f6a31f3825c0cb050e7baee23ee21fcd8
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-03 22:58:34 +0100

    implement conditional navigation options

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,10 +1,16 @@
 import { NavLink } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import classes from './Navigation.module.css'
 import BurgerIcon from '../assets/burger_icon.svg'
 
 function Navigation() {
 	const [open, setOpen] = useState(false)
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+	useEffect(() => {
+		const token = localStorage.getItem('token')
+		token ? setIsLoggedIn(true) : setIsLoggedIn(false)
+	}, [])
 
 	return (
 		<nav className={classes.nav}>
@@ -12,16 +18,43 @@ function Navigation() {
 				<img src={BurgerIcon} alt="burger icon" className={classes['burger-bars']} />
 			</button>
 			<ul className={`${classes.list} ${open ? `${classes.opened}` : `${classes.closed}`}`}>
-				<li className={classes['list-item']}>
-					<NavLink to="/" className={({isActive}) => `${classes['list-item-link']} ${isActive ? classes.active : ''}`}>
-						Home
-					</NavLink>
-				</li>
-				<li className={classes['list-item']}>
-					<NavLink to="/login" className={({isActive}) => `${classes['list-item-link']} ${isActive ? classes.active : ''}`}>
-						Login
-					</NavLink>
-				</li>
+				{!isLoggedIn && (
+					<>
+						<li className={classes['list-item']}>
+							<NavLink
+								to="/"
+								className={({ isActive }) => `${classes['list-item-link']} ${isActive ? classes.active : ''}`}>
+								Home
+							</NavLink>
+						</li>
+						<li className={classes['list-item']}>
+							<NavLink
+								to="/login"
+								className={({ isActive }) => `${classes['list-item-link']} ${isActive ? classes.active : ''}`}>
+								Login
+							</NavLink>
+						</li>
+					</>
+				)}
+
+				{isLoggedIn && (
+					<>
+						<li className={classes['list-item']}>
+							<NavLink
+								to="/dashboard"
+								className={({ isActive }) => `${classes['list-item-link']} ${isActive ? classes.active : ''}`}>
+								Dashboard
+							</NavLink>
+						</li>
+						<li className={classes['list-item']}>
+							<NavLink
+								to="/newemployee"
+								className={({ isActive }) => `${classes['list-item-link']} ${isActive ? classes.active : ''}`}>
+								New employee
+							</NavLink>
+						</li>
+					</>
+				)}
 			</ul>
 		</nav>
 	)
