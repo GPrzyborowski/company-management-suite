c6923195b3a5598b6f77e66874a7b20337bf50cc
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-03 23:47:57 +0100

    add register to app router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
@@ -18,6 +19,7 @@ function App() {
 						</ProtectedRoute>
 					}></Route>
 				<Route path="/login" element={<Login />}></Route>
+				<Route path="/register" element={<Register />}></Route>
 			</Routes>
 		</BrowserRouter>
 	)
