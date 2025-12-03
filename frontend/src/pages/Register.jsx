11bc107afe5085aff65f53372eeaedbd4a9163bf
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-03 23:45:34 +0100

    create register route

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import Navigation from '../components/Navigation'
+import RegisterForm from '../components/RegisterForm'
+
+function Register() {
+    return (
+        <>
+            <Navigation />
+            <RegisterForm />
+        </>
+    )
+}
+
+export default Register
