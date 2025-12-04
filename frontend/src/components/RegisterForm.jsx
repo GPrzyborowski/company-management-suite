359ea771f113dc3b17b29c0243e7663e3d38f1d9
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-04 23:46:13 +0100

    create registration form

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,8 +1,25 @@
 import classes from './RegisterForm.module.css'
 
-function RegisterForm() {
+function RegisterForm({ onSubmit, login, setLogin, password, setPassword, phone, setPhone }) {
+
+    const handleSubmit = e => {
+		e.preventDefault()
+		onSubmit()
+	}
+
     return (
-        <main className={classes.main}></main>
+        <main className={classes.main}>
+            <h1 className={classes.header}>Register a new admin account.</h1>
+            <form onSubmit={handleSubmit} className={classes.form}>
+                <label htmlFor="login" className={classes['form-label']}>Login:</label>
+                <input type="text" id="login" className={classes['form-input']} value={login} onChange={e => setLogin(e.target.value)} /> <br />
+                <label htmlFor="password" className={classes['form-label']}>Password:</label>
+                <input type="password" id="password" className={classes['form-input']} value={password} onChange={e => setPassword(e.target.value)} /> <br />
+                <label htmlFor="phone" className={classes['form-label']}>Phone number:</label>
+                <input type="text" id="phone" className={classes['form-input']} value={phone} onChange={e => setPhone(e.target.value)} /> <br />
+                <button type="submit" className={classes['form-btn']}>Register</button>
+            </form>
+        </main>
     )
 }
 
