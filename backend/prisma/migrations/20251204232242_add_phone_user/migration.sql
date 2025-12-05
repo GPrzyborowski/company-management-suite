ace582fb682014ea964cafc546647222b315a5e8
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-05 01:11:24 +0100

    add phone column to users table

---
+++ backend/prisma/migrations/20251204232242_add_phone_user/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL DEFAULT '',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_User" ("createdAt", "id", "login", "password") SELECT "createdAt", "id", "login", "password" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
