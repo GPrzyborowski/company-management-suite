24a5025dfe9d2b12c592ba44b41a50358d3797d7
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-05 01:15:50 +0100

    implement admin registration logic

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,13 +1,45 @@
+import { useState } from 'react'
 import Navigation from '../components/Navigation'
 import RegisterForm from '../components/RegisterForm'
 
 function Register() {
-    return (
-        <>
-            <Navigation />
-            <RegisterForm />
-        </>
-    )
+
+	const [login, setLogin] = useState('')
+	const [password, setPassword] = useState('')
+	const [phone, setPhone] = useState('')
+
+        const localEndpoint = 'http://localhost:5000/api/auth/registeradmin'
+
+    	async function submit() {
+		const res = await fetch(localEndpoint, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ login, password, phone }),
+		})
+
+		const data = await res
+
+		if (res.ok) {
+			window.location.href = '/'
+		} else {
+			console.error(data.error)
+		}
+	}
+
+	return (
+		<>
+			<Navigation />
+			<RegisterForm
+				onSubmit={submit}
+				login={login}
+				setLogin={setLogin}
+				password={password}
+				setPassword={setPassword}
+				phone={phone}
+				setPhone={setPhone}
+			/>
+		</>
+	)
 }
 
 export default Register
