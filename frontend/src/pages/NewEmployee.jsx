57a572ae02cb2bb14bc053fb9202c49ab70bab04
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-07 05:39:41 +0100

    create employee registration route

---
+++ frontend/src/pages/NewEmployee.jsx
@@ -0,0 +1,13 @@
+import NewEmployeeForm from '../components/NewEmployeeForm'
+import Navigation from '../components/Navigation'
+
+function NewEmployee() {
+    return(
+        <>
+            <Navigation />
+            <NewEmployeeForm />
+        </>
+    )
+}
+
+export default NewEmployee
