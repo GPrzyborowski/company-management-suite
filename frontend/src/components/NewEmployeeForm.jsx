0bfd186c3f19c923a7226deb3e1083319d55b59c
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-07 05:41:04 +0100

    create employee registration form

---
+++ frontend/src/components/NewEmployeeForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import classes from './NewEmployeeForm.module.css'
+
+function NewEmployeeForm() {
+	const handleSubmit = e => {
+		e.preventDefault()
+		onSubmit()
+	}
+
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [phone, setPhone] = useState('')
+
+	return (
+		<main className={classes.main}>
+			<h1 className={classes.header}>Register a new admin account.</h1>
+			<form onSubmit={handleSubmit} className={classes.form}>
+				<label htmlFor="name" className={classes['form-label']}>
+					First name:
+				</label>
+				<input
+					type="text"
+					id="name"
+					className={classes['form-input']}
+					value={name}
+					onChange={e => setName(e.target.value)}
+				/>{' '}
+				<br />
+				<label htmlFor="surname" className={classes['form-label']}>
+					Surname:
+				</label>
+				<input
+					type="text"
+					id="surname"
+					className={classes['form-input']}
+					value={surname}
+					onChange={e => setSurname(e.target.value)}
+				/>{' '}
+				<br />
+				<label htmlFor="phone" className={classes['form-label']}>
+					Phone number:
+				</label>
+				<input
+					type="text"
+					id="phone"
+					className={classes['form-input']}
+					value={phone}
+					onChange={e => setPhone(e.target.value)}
+				/>{' '}
+				<br />
+				<button type="submit" className={classes['form-btn']}>
+					Register
+				</button>
+			</form>
+		</main>
+	)
+}
+
+export default NewEmployeeForm
