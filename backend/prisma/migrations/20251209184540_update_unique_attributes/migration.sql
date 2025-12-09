3c020e3890800f1685756a244e9b10fb6343971a
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-09 19:50:45 +0100

    update unique attributes

---
+++ backend/prisma/migrations/20251209184540_update_unique_attributes/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `phoneNumber` to the `Admin` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Admin" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL
+);
+INSERT INTO "new_Admin" ("id", "login", "password") SELECT "id", "login", "password" FROM "Admin";
+DROP TABLE "Admin";
+ALTER TABLE "new_Admin" RENAME TO "Admin";
+CREATE UNIQUE INDEX "Admin_login_key" ON "Admin"("login");
+CREATE UNIQUE INDEX "Admin_phoneNumber_key" ON "Admin"("phoneNumber");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
