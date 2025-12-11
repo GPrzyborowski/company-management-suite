e0ad64744102e5dccbf49b77297cb501496b2453
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-12 00:11:08 +0100

    create submit function and pass as a prop

--- frontend/src/pages/NewEmployee.jsx
+++ frontend/src/pages/NewEmployee.jsx
@@ -2,12 +2,30 @@ import NewEmployeeForm from '../components/NewEmployeeForm'
 import Navigation from '../components/Navigation'
 
 function NewEmployee() {
-    return(
-        <>
-            <Navigation />
-            <NewEmployeeForm />
-        </>
-    )
+	const localEndpoint = 'http://localhost:5000/api/auth/registeremployee'
+
+	async function submit(dataToSend) {
+		const res = await fetch(localEndpoint, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(dataToSend),
+		})
+
+		const data = await res.json()
+
+		if (res.ok) {
+			console.log('Success.')
+		} else {
+			console.error(data)
+		}
+	}
+
+	return (
+		<>
+			<Navigation />
+			<NewEmployeeForm onSubmit={submit}/>
+		</>
+	)
 }
 
 export default NewEmployee
