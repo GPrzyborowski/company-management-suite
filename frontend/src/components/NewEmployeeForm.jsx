80c70ec6496e109e724cbfaa332a7be69d375065
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-12 00:09:51 +0100

    add input containers, state management and data submition

--- frontend/src/components/NewEmployeeForm.jsx
+++ frontend/src/components/NewEmployeeForm.jsx
@@ -2,13 +2,50 @@ import { useState } from 'react'
 import classes from './NewEmployeeForm.module.css'
 import InputContainer from './InputContainer'
 
-function NewEmployeeForm() {
+function NewEmployeeForm({ onSubmit }) {
 	const handleSubmit = e => {
 		e.preventDefault()
-		onSubmit()
+		const dataToSend = {
+			firstName,
+			lastName,
+			birthDate,
+			jobStartDate,
+			email,
+			phone,
+			socialSecurity,
+			address: {
+				"country": country,
+				"city": city,
+				"postalCode": postalCode,
+				"street": street,
+				"buildingNumber": buildingNumber,
+				"apartmentNumber": apartmentNumber
+			},
+			bankNumber,
+			salaryRate,
+			contractType,
+			role,
+		}
+		onSubmit(dataToSend)
 	}
 
 	const [firstName, setFirstName] = useState('')
+	const [lastName, setLastName] = useState('')
+	const [birthDate, setBirthDate] = useState('')
+	const [jobStartDate, setJobStartDate] = useState('')
+	const [email, setEmail] = useState('')
+	const [phone, setPhone] = useState('')
+	const [socialSecurity, setSocialSecurity] = useState('')
+	const [country, setCountry] = useState('')
+	const [city, setCity] = useState('')
+	const [postalCode, setPostalCode] = useState('')
+	const [street, setStreet] = useState('')
+	const [buildingNumber, setBuildingNumber] = useState('')
+	const [apartmentNumber, setApartmentNumber] = useState('')
+	const [bankNumber, setBankNumber] = useState('')
+	const [salaryRate, setSalaryRate] = useState('')
+	const [contractType, setContractType] = useState('')
+	const [role, setRole] = useState('')
 
 	return (
 		<main className={classes.main}>
@@ -20,8 +57,155 @@ function NewEmployeeForm() {
 					type="text"
 					id="firstName"
 					value={firstName}
-					onChange={setFirstName}
+					onChange={e => setFirstName(e.target.value)}
 				/>
+
+				<InputContainer
+					htmlFor="lastName"
+					labelText="Last name:"
+					type="text"
+					id="lastName"
+					value={lastName}
+					onChange={e => setLastName(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="birthDate"
+					labelText="Birth date:"
+					type="date"
+					id="birthDate"
+					value={birthDate}
+					onChange={e => setBirthDate(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="jobStartDate"
+					labelText="Job start date:"
+					type="date"
+					id="jobStartDate"
+					value={jobStartDate}
+					onChange={e => setJobStartDate(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="email"
+					labelText="Email:"
+					type="text"
+					id="email"
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="phone"
+					labelText="Phone number:"
+					type="text"
+					id="phone"
+					value={phone}
+					onChange={e => setPhone(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="socialSecurity"
+					labelText="Social security number:"
+					type="text"
+					id="socialSecurity"
+					value={socialSecurity}
+					onChange={e => setSocialSecurity(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="country"
+					labelText="Country:"
+					type="text"
+					id="country"
+					value={country}
+					onChange={e => setCountry(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="city"
+					labelText="City:"
+					type="text"
+					id="city"
+					value={city}
+					onChange={e => setCity(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="postalCode"
+					labelText="Postal code:"
+					type="text"
+					id="postalCode"
+					value={postalCode}
+					onChange={e => setPostalCode(e.target.value)}
+				/>
+
+				
+				<InputContainer
+					htmlFor="street"
+					labelText="Street:"
+					type="text"
+					id="street"
+					value={street}
+					onChange={e => setStreet(e.target.value)}
+				/>
+
+				
+				<InputContainer
+					htmlFor="buildingNumber"
+					labelText="Building number:"
+					type="text"
+					id="buildingNumber"
+					value={buildingNumber}
+					onChange={e => setBuildingNumber(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="apartmentNumber"
+					labelText="Apartment number:"
+					type="text"
+					id="apartmentNumber"
+					value={apartmentNumber}
+					onChange={e => setApartmentNumber(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="bankNumber"
+					labelText="Bank account number:"
+					type="text"
+					id="bankNumber"
+					value={bankNumber}
+					onChange={e => setBankNumber(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="salaryRate"
+					labelText="Salary rate:"
+					type="text"
+					id="salaryRate"
+					value={salaryRate}
+					onChange={e => setSalaryRate(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="contractType"
+					labelText="Contract type:"
+					type="text"
+					id="contractType"
+					value={contractType}
+					onChange={e => setContractType(e.target.value)}
+				/>
+
+				<InputContainer
+					htmlFor="role"
+					labelText="Role:"
+					type="text"
+					id="role"
+					value={role}
+					onChange={e => setRole(e.target.value)}
+				/>
+
 				<button type="submit" className={classes['form-btn']}>
 					Submit
 				</button>
