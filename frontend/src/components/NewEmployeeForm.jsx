382929d6505d1ccef3bfa14efdb39878d1ee6d92
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-11 01:14:14 +0100

    add temporary input component

--- frontend/src/components/NewEmployeeForm.jsx
+++ frontend/src/components/NewEmployeeForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import classes from './NewEmployeeForm.module.css'
+import InputContainer from './InputContainer'
 
 function NewEmployeeForm() {
 	const handleSubmit = e => {
@@ -7,49 +8,22 @@ function NewEmployeeForm() {
 		onSubmit()
 	}
 
-    const [name, setName] = useState('')
-    const [surname, setSurname] = useState('')
-    const [phone, setPhone] = useState('')
+	const [firstName, setFirstName] = useState('')
 
 	return (
 		<main className={classes.main}>
 			<h1 className={classes.header}>Register a new employee.</h1>
 			<form onSubmit={handleSubmit} className={classes.form}>
-				<label htmlFor="name" className={classes['form-label']}>
-					First name:
-				</label>
-				<input
+				<InputContainer
+					htmlFor="firstName"
+					labelText="First name:"
 					type="text"
-					id="name"
-					className={classes['form-input']}
-					value={name}
-					onChange={e => setName(e.target.value)}
-				/>{' '}
-				<br />
-				<label htmlFor="surname" className={classes['form-label']}>
-					Surname:
-				</label>
-				<input
-					type="text"
-					id="surname"
-					className={classes['form-input']}
-					value={surname}
-					onChange={e => setSurname(e.target.value)}
-				/>{' '}
-				<br />
-				<label htmlFor="phone" className={classes['form-label']}>
-					Phone number:
-				</label>
-				<input
-					type="text"
-					id="phone"
-					className={classes['form-input']}
-					value={phone}
-					onChange={e => setPhone(e.target.value)}
-				/>{' '}
-				<br />
+					id="firstName"
+					value={firstName}
+					onChange={setFirstName}
+				/>
 				<button type="submit" className={classes['form-btn']}>
-					Register
+					Submit
 				</button>
 			</form>
 		</main>
