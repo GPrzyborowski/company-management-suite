2fc079a31197266f9d28266979ad25701258dc9a
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-14 02:04:56 +0100

    create employees list page

---
+++ frontend/src/pages/Employees.jsx
@@ -0,0 +1,13 @@
+import Navigation from '../components/Navigation'
+import EmployeesMain from '../components/EmployeesMain'
+
+function Employees() {
+	return (
+		<>
+			<Navigation />
+            <EmployeesMain />
+		</>
+	)
+}
+
+export default Employees
