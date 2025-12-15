9628c8934e8bb1cb568a0b4ce8c702d278a03c26
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-15 02:44:57 +0100

    add class to login input field

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -18,7 +18,7 @@ function LoginForm({ onSubmit, login, setLogin, password, setPassword }) {
 		<main className={classes.container}>
 			<form onSubmit={handleSubmit} className={classes.form}>
 				<label htmlFor="login" className={classes['form-label']}>Login:</label>
-				<input type="text" id="login" className={classes['form-input']} value={login} onChange={e => setLogin(e.target.value)} /> <br />
+				<input type="text" id="login" className={`${classes['form-input']} ${classes.login}`} value={login} onChange={e => setLogin(e.target.value)} /> <br />
 				<label htmlFor="password" className={classes['form-label']}>Password:</label>
 				<input type="password" id="password" className={classes['form-input']} value={password} onChange={e => setPassword(e.target.value)} /> <br />
 				<button type="submit" className={classes['form-btn']}>Log in</button>
