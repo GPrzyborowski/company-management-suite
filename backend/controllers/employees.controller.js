88bf23585b9fac64b32d92cf109436a904c80d1c
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-16 02:58:04 +0100

    implement controller to fetch employees data

---
+++ backend/controllers/employees.controller.js
@@ -0,0 +1,29 @@
+import prisma from '../src/config/prisma.js'
+
+export const getEmployees = async (req, res) => {
+    try {
+        const employees = await prisma.employee.findMany()
+        res.json(employees)
+    } catch(err) {
+        console.error(err)
+        res.status(500).json({message: 'Error.'})
+    }
+}
+
+export const getEmployeeById = async (req, res) => {
+    const id = Number(req.params.id)
+    const employee = await prisma.employee.findUnique({where: {id}})
+    if(!employee) {
+        return res.status(404).json({message: 'User not found.'})
+    }
+    res.json(employee)
+}
+
+export const updateEmployee = async (req, res) => {
+    const id = Number(req.params.id)
+    const employee = await prisma.employee.update({
+        where: {id},
+        data: req.body
+    })
+    res.json(employee)
+}
