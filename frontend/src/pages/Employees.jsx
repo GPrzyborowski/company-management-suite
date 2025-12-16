f87d9f754c15efd4dfe445ca0055429867dbd89f
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-16 03:01:12 +0100

    fetch employees data and render cards

--- frontend/src/pages/Employees.jsx
+++ frontend/src/pages/Employees.jsx
@@ -1,11 +1,46 @@
 import Navigation from '../components/Navigation'
-import EmployeesMain from '../components/EmployeesMain'
+import EmployeeCard from '../components/EmployeeCard'
+import { useState, useEffect } from 'react'
 
 function Employees() {
+	const [employees, setEmployees] = useState([])
+	const localEndpoint = `http://localhost:5000/api/employees`
+
+	useEffect(() => {
+		fetch(localEndpoint, {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+		})
+			.then(async res => {
+				if(!res) {
+					throw new Error(res.status)
+				}
+				return res.json()
+			})
+			.then((data) => {
+				if(Array.isArray(data)) {
+					setEmployees(data)
+				} else {
+					setEmployees([])
+					console.log(data);
+					console.error("Wrong data format.")
+				}
+			})
+			.catch((err) => {
+				setEmployees([])
+				console.error(err)
+			})
+	}, [])
+
 	return (
 		<>
 			<Navigation />
-            <EmployeesMain />
+			<div className="container">
+				{employees.map(element => {
+					return <EmployeeCard key={element.id} employee={element} />
+				})}
+			</div>
 		</>
 	)
 }
