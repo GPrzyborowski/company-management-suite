f5fa95587367d495b2d25724c2f6500bee3e12b2
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-21 04:31:47 +0100

    render employee information component

---
+++ frontend/src/pages/EmployeeInformation.jsx
@@ -0,0 +1,13 @@
+import Employee from '../components/Employee'
+import Navigation from '../components/Navigation'
+
+function EmployeeInformation() {
+	return (
+		<>
+			<Navigation />
+			<Employee />
+		</>
+	)
+}
+
+export default EmployeeInformation
