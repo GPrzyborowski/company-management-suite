6542d47255f63ac1c298f3fd0082439c1d734843
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-21 03:46:07 +0100

    remove employees routing configuration

--- frontend/src/router.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Employees from './pages/Employees'
-
-export const router = createBrowserRouter([
-    {
-        path: '/employees',
-        element: <Employees />
-    }
-])
