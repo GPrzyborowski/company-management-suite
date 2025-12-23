8abbadd66252b7a1101fd254e898b94bd77246c3
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-23 23:36:02 +0100

    add new information and labels

--- frontend/src/components/Employee.jsx
+++ frontend/src/components/Employee.jsx
@@ -21,13 +21,22 @@ function Employee() {
 			})
 	}, [id])
 
+	const birthDate = new Date(employee.birthDate)
+	const birthDateFormatted = birthDate.toLocaleDateString('pl-PL', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric'
+	})
+
 	return (
 		<main className={classes.main}>
 			<div className={classes.box}>
 				<div className={classes['info-box']}>
-					<h1 className={classes.name}>
-						{employee.firstName} {employee.lastName}
-					</h1>
+					<p className={classes.info}>
+						<span className={classes['info-label']}>Name:</span> {employee.firstName} {employee.lastName}
+					</p>
+					<p className={classes.info}><span className={classes['info-label']}>Birth date: </span>{birthDateFormatted}</p>
+					<p className={classes.info}><span className={classes['info-label']}>Social number: </span>{employee.socialNumber}</p>
 				</div>
 				<div className={classes['work-box']}>
 					<p>test</p>
