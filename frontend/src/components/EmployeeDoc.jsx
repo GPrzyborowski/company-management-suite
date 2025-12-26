bcf3a0e882b57bcb3e1d60158fca966bf1e6602e
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-26 23:51:27 +0100

    create employee document component

---
+++ frontend/src/components/EmployeeDoc.jsx
@@ -0,0 +1,11 @@
+import classes from './EmployeeDoc.module.css'
+
+function EmployeeDoc() {
+    return (
+        <div className={classes.container}>
+
+        </div>        
+    )
+}
+
+export default EmployeeDoc
