8804e90abfd58f29b22c6006bb74d61b1ead3005
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-27 00:29:13 +0100

    create placeholders for fetched data and add icons

--- frontend/src/components/EmployeeDoc.jsx
+++ frontend/src/components/EmployeeDoc.jsx
@@ -1,11 +1,14 @@
 import classes from './EmployeeDoc.module.css'
 
 function EmployeeDoc() {
-    return (
-        <div className={classes.container}>
-
-        </div>        
-    )
+	return (
+		<div className={classes.container}>
+			<p className={classes.header}></p>
+			<p className={classes.date}></p>
+			<img src="/download.svg" alt="" className={`${classes.icon} ${classes['icon-download']}`}/>
+			<img src="/delete.svg" alt="" className={`${classes.icon} ${classes['icon-delete']}`}/>
+		</div>
+	)
 }
 
 export default EmployeeDoc
