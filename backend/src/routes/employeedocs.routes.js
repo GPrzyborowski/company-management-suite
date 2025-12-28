6501897ee340dd811a201af4471a453067b78cf3
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-29 00:37:16 +0100

    add post route to upload documents

--- backend/src/routes/employeedocs.routes.js
+++ backend/src/routes/employeedocs.routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import fs from 'fs'
 import prisma from '../config/prisma.js'
 import { auth } from '../middleware/auth.js'
+import { uploadEmployeeDoc } from '../middleware/uploademployeedoc.js'
 
 const router = express.Router()
 
@@ -20,3 +21,27 @@ router.get('/employees/:id/documents', auth, async (req, res) => {
     })
     res.json(documents)
 })
+
+router.post('/employees/:id/documents', auth, uploadEmployeeDoc.single('file'), async (req, res) => {
+    const employeeId = req.params.id
+    const document = await prisma.employeedocument.create({
+        data: {
+            employeeId,
+            fileName: req.file.originalname,
+            storedName: req.file.filename,
+            mimeType: req.file.mimetype,
+            size: req.file.size,
+            filePath: req.file.path
+        },
+        select: {
+            id: true,
+            fileName: true,
+            uploadedAt: true,
+            mimeType: true,
+            size: true
+        }
+    })
+    res.status(201).json(document)
+})
+
+export default router
