98165112274440e94300b801455fdcbd70fbaab2
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2025-12-29 00:27:26 +0100

    implement middleware for documents upload

---
+++ backend/src/middleware/uploademployeedoc.js
@@ -0,0 +1,21 @@
+import multer from 'multer'
+import fs from 'fs'
+import path from 'path'
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const employeeId = req.params.id
+        const dir = path.join(process.cwd(), 'uploads', 'employees', employeeId)
+        fs.mkdirSync(dir, {resursive: true})
+        cb(null, dir)
+    },
+    filename: (req, file, cb) => {
+        const safeOriginal = file.originalname.replace(/\s+/g, "_")
+        cb(null, `${Date.now()}_${safeOriginal}`)
+    }
+})
+
+export const uploadEmployeeDoc = multer({
+    storage,
+    limits: {fileSize: 10 * 1024 * 1024},
+})
