66b0486f5d7c979eccfbffd3c51eda2c190ffa19
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-01 02:03:43 +0100

    add header and date props for data rendering

--- frontend/src/components/EmployeeDoc.jsx
+++ frontend/src/components/EmployeeDoc.jsx
@@ -1,10 +1,10 @@
 import classes from './EmployeeDoc.module.css'
 
-function EmployeeDoc() {
+function EmployeeDoc({header, date}) {
 	return (
 		<div className={classes.container}>
-			<p className={classes.header}></p>
-			<p className={classes.date}></p>
+			<p className={classes.header}>{header}</p>
+			<p className={classes.date}>Uploaded: {date}</p>
 			<img src="/download.svg" alt="" className={`${classes.icon} ${classes['icon-download']}`}/>
 			<img src="/delete.svg" alt="" className={`${classes.icon} ${classes['icon-delete']}`}/>
 		</div>
