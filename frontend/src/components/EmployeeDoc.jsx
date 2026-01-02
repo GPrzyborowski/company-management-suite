a0b722951b57ea1ad591af372069de0878298536
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-02 20:37:01 +0100

    add download function prop

--- frontend/src/components/EmployeeDoc.jsx
+++ frontend/src/components/EmployeeDoc.jsx
@@ -1,12 +1,12 @@
 import classes from './EmployeeDoc.module.css'
 
-function EmployeeDoc({header, date}) {
+function EmployeeDoc({header, date, onDownload}) {
 	return (
 		<div className={classes.container}>
 			<p className={classes.header}>{header}</p>
 			<p className={classes.date}>Uploaded: {date}</p>
-			<img src="/download.svg" alt="" className={`${classes.icon} ${classes['icon-download']}`}/>
-			<img src="/delete.svg" alt="" className={`${classes.icon} ${classes['icon-delete']}`}/>
+			<button onClick={onDownload}><img src="/download.svg" alt="" className={`${classes.icon} ${classes['icon-download']}`}/></button>
+			<button><img src="/delete.svg" alt="" className={`${classes.icon} ${classes['icon-delete']}`}/></button>
 		</div>
 	)
 }
