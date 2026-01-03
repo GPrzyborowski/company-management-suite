1224ce19d3c85f3068ca0962cbc58842f2f72ae7
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-04 00:50:32 +0100

    implement conditional welcome message rendering

--- frontend/src/components/DashboardMain.jsx
+++ frontend/src/components/DashboardMain.jsx
@@ -3,29 +3,37 @@ import { jwtDecode } from 'jwt-decode'
 import classes from './DashboardMain.module.css'
 
 function DashboardMain() {
+	const [login, setLogin] = useState('')
 
-    const [login, setLogin] = useState('')
+	useEffect(() => {
+		const token = localStorage.getItem('token')
+		if (!token) {
+			return
+		}
+		try {
+			const decoded = jwtDecode(token)
+			setLogin(decoded.login)
+		} catch (err) {
+			console.error(err)
+		}
+	}, [])
 
-    useEffect(() => {
-        const token = localStorage.getItem('token')
-        if(!token) {
-            return
+    const getGreeting = () => {
+        const hour = new Date().getHours()
+        if((hour > 4) && (hour < 12)) {
+            return `Good morning ${login}.`
+        } else if((hour > 12) && (hour < 19)) {
+            return `Good afternoon ${login}.`
+        } else {
+            return `Good evening ${login}.`
         }
-        try {
-            const decoded = jwtDecode(token)
-            setLogin(decoded.login)
-        } catch(err) {
-            console.error(err)
-        }
-    }, [])
+    }
 
-    
-
-    return (
-        <main className={classes['dashboard-container']}>
-            <h1 className={classes['main-header']}>Hello {login}</h1>
-        </main>
-    )
+	return (
+		<main className={classes['dashboard-container']}>
+			<h1 className={classes['main-header']}>{getGreeting()}</h1>
+		</main>
+	)
 }
 
 export default DashboardMain
