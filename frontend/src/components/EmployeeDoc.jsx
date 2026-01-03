a5c02e9db24028f8ba6e95df6690038b59c2bea8
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-03 02:56:58 +0100

    add delete function prop

--- frontend/src/components/EmployeeDoc.jsx
+++ frontend/src/components/EmployeeDoc.jsx
@@ -1,12 +1,12 @@
 import classes from './EmployeeDoc.module.css'
 
-function EmployeeDoc({header, date, onDownload}) {
+function EmployeeDoc({header, date, onDownload, onDelete}) {
 	return (
 		<div className={classes.container}>
 			<p className={classes.header}>{header}</p>
 			<p className={classes.date}>Uploaded: {date}</p>
 			<button onClick={onDownload}><img src="/download.svg" alt="" className={`${classes.icon} ${classes['icon-download']}`}/></button>
-			<button><img src="/delete.svg" alt="" className={`${classes.icon} ${classes['icon-delete']}`}/></button>
+			<button onClick={onDelete}><img src="/delete.svg" alt="" className={`${classes.icon} ${classes['icon-delete']}`}/></button>
 		</div>
 	)
 }
