d3d3434a04d91aca00a0bb20d5ccd24b53b0f642
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-05 20:57:59 +0100

    add document size prop and paragraph

--- frontend/src/components/EmployeeDoc.jsx
+++ frontend/src/components/EmployeeDoc.jsx
@@ -1,10 +1,11 @@
 import classes from './EmployeeDoc.module.css'
 
-function EmployeeDoc({header, date, onDownload, onDelete}) {
+function EmployeeDoc({header, date, size, onDownload, onDelete}) {
 	return (
 		<div className={classes.container}>
 			<p className={classes.header}>{header}</p>
 			<p className={classes.date}>Uploaded: {date}</p>
+			<p className={classes.size}>{size}</p>
 			<button onClick={onDownload} className={classes.btn}><img src="/download.svg" alt="" className={`${classes.icon} ${classes['icon-download']}`}/></button>
 			<button onClick={onDelete} className={classes.btn}><img src="/delete.svg" alt="" className={`${classes.icon} ${classes['icon-delete']}`}/></button>
 		</div>
