0b6db1000c55201f728085d6e3aa0ec87da6f0d4
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-09 21:40:31 +0100

    add modal container

--- frontend/src/components/ConfirmModal.jsx
+++ frontend/src/components/ConfirmModal.jsx
@@ -2,11 +2,13 @@ import classes from './ConfirmModal.module.css'
 
 function ConfirmModal() {
 	return (
-		<div className={classes.box}>
-			<p className={classes.paragraph}>Are you sure you want to delete this file?</p>
-			<div className={classes['btn-box']}>
-				<button className={`${classes.btn} ${classes.cancel}`}>Cancel</button>
-				<button className={`${classes.btn} ${classes.confirm}`}>Confirm</button>
+		<div className={classes.modal}>
+			<div className={classes.box}>
+				<p className={classes.paragraph}>Are you sure you want to delete this file?</p>
+				<div className={classes['btn-box']}>
+					<button className={`${classes.btn} ${classes.cancel}`}>Cancel</button>
+					<button className={`${classes.btn} ${classes.confirm}`}>Confirm</button>
+				</div>
 			</div>
 		</div>
 	)
