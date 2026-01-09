35b73f4c5d34b56bad550ce748f363101216d187
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-10 00:51:22 +0100

    add state and click handler props

--- frontend/src/components/ConfirmModal.jsx
+++ frontend/src/components/ConfirmModal.jsx
@@ -1,14 +1,17 @@
 import classes from './ConfirmModal.module.css'
 
-function ConfirmModal() {
+function ConfirmModal({visible, onConfirm, onCancel}) {
+	if(!visible) {
+		return null
+	}
 	return (
-		<div className={classes['modal-overlay']}>
-			<div className={classes.modal}>
+		<div className={classes['modal-overlay']} onClick={onCancel}>
+			<div className={classes.modal} onClick={e => e.stopPropagation()}>
 				<div className={classes.box}>
 					<p className={classes.paragraph}>Are you sure you want to delete this file?</p>
 					<div className={classes['btn-box']}>
-						<button className={`${classes.btn} ${classes.cancel}`}>Cancel</button>
-						<button className={`${classes.btn} ${classes.confirm}`}>Confirm</button>
+						<button className={`${classes.btn} ${classes.cancel}`} onClick={onCancel}>Cancel</button>
+						<button className={`${classes.btn} ${classes.confirm}`} onClick={onConfirm}>Confirm</button>
 					</div>
 				</div>
 			</div>
