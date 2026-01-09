0b09431e2d221572be0ba97bdbbad5f454fb8f10
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-09 21:43:36 +0100

    add modal overlay

--- frontend/src/components/ConfirmModal.jsx
+++ frontend/src/components/ConfirmModal.jsx
@@ -2,12 +2,14 @@ import classes from './ConfirmModal.module.css'
 
 function ConfirmModal() {
 	return (
-		<div className={classes.modal}>
-			<div className={classes.box}>
-				<p className={classes.paragraph}>Are you sure you want to delete this file?</p>
-				<div className={classes['btn-box']}>
-					<button className={`${classes.btn} ${classes.cancel}`}>Cancel</button>
-					<button className={`${classes.btn} ${classes.confirm}`}>Confirm</button>
+		<div className={classes['modal-overlay']}>
+			<div className={classes.modal}>
+				<div className={classes.box}>
+					<p className={classes.paragraph}>Are you sure you want to delete this file?</p>
+					<div className={classes['btn-box']}>
+						<button className={`${classes.btn} ${classes.cancel}`}>Cancel</button>
+						<button className={`${classes.btn} ${classes.confirm}`}>Confirm</button>
+					</div>
 				</div>
 			</div>
 		</div>
