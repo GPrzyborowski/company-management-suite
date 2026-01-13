b5b8325475a173ea127d04c15a07f9410775a25e
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-13 02:50:37 +0100

    fix employee update endpoint function

--- backend/controllers/employees.controller.js
+++ backend/controllers/employees.controller.js
@@ -1,35 +1,52 @@
 import prisma from '../src/config/prisma.js'
 
 export const getEmployees = async (req, res) => {
-    try {
-        const employees = await prisma.employee.findMany()
-        res.json(employees)
-    } catch(err) {
-        console.error(err)
-        res.status(500).json({message: 'Error.'})
-    }
+	try {
+		const employees = await prisma.employee.findMany()
+		res.json(employees)
+	} catch (err) {
+		console.error(err)
+		res.status(500).json({ message: 'Error.' })
+	}
 }
 
 export const getEmployeeById = async (req, res) => {
-    const id = Number(req.params.id)
-    const employee = await prisma.employee.findUnique({
-        where: {id},
-        include: {
-            address: true
-        }
-    })
-    
-    if(!employee) {
-        return res.status(404).json({message: 'User not found.'})
-    }
-    res.json(employee)
+	const id = Number(req.params.id)
+	const employee = await prisma.employee.findUnique({
+		where: { id },
+		include: {
+			address: true,
+		},
+	})
+
+	if (!employee) {
+		return res.status(404).json({ message: 'User not found.' })
+	}
+	res.json(employee)
 }
 
 export const updateEmployee = async (req, res) => {
-    const id = Number(req.params.id)
-    const employee = await prisma.employee.update({
-        where: {id},
-        data: req.body
-    })
-    res.json(employee)
+	try {
+		const id = Number(req.params.id)
+		const { address, birthDate, jobStartDate, ...rest } = req.body
+
+		const employee = await prisma.employee.update({
+			where: { id },
+			data: {
+				...rest,
+                birthDate: birthDate ? new Date(birthDate) : undefined,
+                jobStart: jobStartDate ? new Date(jobStartDate) : undefined,
+                address: {
+                    update: address
+                }
+			},
+			include: {
+				address: true,
+			},
+		})
+		res.json(employee)
+	} catch (err) {
+		console.error(err)
+		res.status(500).json({ message: 'Update failed.' })
+	}
 }
