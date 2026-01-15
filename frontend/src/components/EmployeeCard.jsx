66a25d3d7b11c093ecc94eada2f709a1013cab50
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-15 20:43:04 +0100

    replace role paragraph with mail container

--- frontend/src/components/EmployeeCard.jsx
+++ frontend/src/components/EmployeeCard.jsx
@@ -1,19 +1,25 @@
 import classes from './EmployeeCard.module.css'
 import { useNavigate } from 'react-router-dom'
 
-function EmployeeCard({employee}) {
-
+function EmployeeCard({ employee }) {
 	const navigate = useNavigate()
 
 	return (
-		<div className={classes.card} onClick={() => {
-			navigate(`/employees/${employee.id}`)
-		}}>
-			<p className={classes.name}>{employee.firstName} {employee.lastName}</p>
-			<div className={classes['card-role-phone']}>
-				<p className={classes.role}>{employee.position}</p>
+		<div
+			className={classes.card}
+			onClick={() => {
+				navigate(`/employees/${employee.id}`)
+			}}>
+			<p className={classes.name}>
+				{employee.firstName} {employee.lastName}
+			</p>
+			<div className={classes['card-mail-phone']}>
+				<div className={classes['card-email']}>
+					<img src="/mail.svg" alt="mail icon" className={classes['mail-icon']} />
+					<p>{employee.phoneNumber}</p>
+				</div>
 				<div className={classes['card-phone']}>
-                    <img src="/phone.svg" alt="phone icon" className={classes['phone-icon']}/>
+					<img src="/phone.svg" alt="phone icon" className={classes['phone-icon']} />
 					<p>{employee.phoneNumber}</p>
 				</div>
 			</div>
