6e3dfae8add94c087ffd6cc2cf37836e249aa6e9
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-18 00:56:29 +0100

    create login screen with banner

---
+++ mobile/src/components/Banner.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+function Banner() {
+    return (
+        <Text>Provide login code.</Text>
+    )
+}
+
+export default Banner
---
+++ mobile/src/screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import Banner from "../components/Banner"
+
+function LoginScreen() {
+    return (
+        <>
+            <Banner />
+        </>
+    )
+}
+
+export default LoginScreen
