e0e9d6e6f44548097b442cf797a7e7482199ae90
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-19 20:58:28 +0100

    implement navigation structure

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './src/screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import HomeScreen from './src/screens/HomeScreen'
+import HostScreen from './src/screens/HostScreen'
+import LoginScreen from './src/screens/LoginScreen'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginScreen />
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Login" component={LoginScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
