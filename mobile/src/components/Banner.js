4860c14855fb43c091e47f7992280e98c039a9a5
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-20 04:43:03 +0100

    add text prop and top padding

--- mobile/src/components/Banner.js
+++ mobile/src/components/Banner.js
@@ -1,9 +1,19 @@
-import { Text } from "react-native"
+import { Text, View, StyleSheet } from 'react-native'
 
-function Banner() {
-    return (
-        <Text>Provide login code.</Text>
-    )
+function Banner({text}) {
+	return (
+		<View>
+			<Text style={styles.headerText}>{text}</Text>
+		</View>
+	)
 }
+
+const styles = StyleSheet.create({
+	headerText: {
+        paddingTop: 32,
+		textAlign: 'center',
+		fontSize: 18,
+	},
+})
 
 export default Banner
