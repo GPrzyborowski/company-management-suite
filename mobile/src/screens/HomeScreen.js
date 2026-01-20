44468c9d85ead81c717a60fac3286ce99a8dfd4e
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-20 04:22:41 +0100

    change screen structure and implement styling

--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -1,15 +1,63 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native'
 
 function HomeScreen({ navigation }) {
 	return (
-		<View>
-			<Text>Choose what you would like to do.</Text>
-			<Button title="Employee Login"
-                onPress={() => navigation.navigate('Login')}
-            ></Button>
-			<Button title="Configure as Host"></Button>
+		<View style={styles.container}>
+			<View style={styles.headerContainer}>
+				<Text style={styles.header}>Company management suite.</Text>
+				<Text style={styles.headerText}>Choose what you would like to do.</Text>
+			</View>
+			<View style={styles.btnContainer}>
+				<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Login')}>
+					<Text style={styles.btnText}>Employee Login</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Host')}>
+					<Text style={styles.btnText}>Configure as Host</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	headerContainer: {
+		paddingTop: 32,
+	},
+	header: {
+		fontWeight: 'bold',
+		paddingBottom:  24,
+		textAlign: 'center',
+		fontSize: 24,
+	},
+	headerText: {
+		textAlign: 'center',
+		fontSize: 18,
+	},
+	btnContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingBottom: 56,
+	},
+	btn: {
+		backgroundColor: '#0f52ba',
+		paddingVertical: 25,
+		paddingHorizontal: 10,
+		borderRadius: 8,
+		width: '80%',
+		alignItems: 'center',
+		marginVertical: 10,
+		elevation: 3,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.25,
+	},
+	btnText: {
+		color: '#FFF',
+	},
+})
 
 export default HomeScreen
