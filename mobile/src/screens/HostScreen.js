1ba06f2980143c89210947b2dedbf914f0834adf
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-21 08:30:19 +0100

    add submit button and implement unified stylesheet

--- mobile/src/screens/HostScreen.js
+++ mobile/src/screens/HostScreen.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import Banner from '../components/Banner'
 import InputBox from '../components/InputBox'
 
@@ -7,8 +7,32 @@ function HostScreen() {
 		<View>
 			<Banner text={'Please provide a host code.'} />
 			<InputBox text="Host code" placeholder="Provide host code here..." />
+			<TouchableOpacity style={styles.btn}>
+				<Text style={styles.btnText}>Submit</Text>
+			</TouchableOpacity>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	btn: {
+		alignSelf: 'center',
+		backgroundColor: '#0f52ba',
+		marginTop: 32,
+		paddingVertical: 22,
+		paddingHorizontal: 10,
+		borderRadius: 8,
+		width: '80%',
+		alignItems: 'center',
+		marginVertical: 10,
+		elevation: 3,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.25,
+	},
+	btnText: {
+		color: '#FFF',
+	},
+})
 
 export default HostScreen
