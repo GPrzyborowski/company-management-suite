7718bac1eee90cb7082f6cb76d152a28f2ac4ac2
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-21 08:16:55 +0100

    add host code input container

--- mobile/src/screens/HostScreen.js
+++ mobile/src/screens/HostScreen.js
@@ -1,9 +1,14 @@
-import { Text } from "react-native"
+import { View } from 'react-native'
+import Banner from '../components/Banner'
+import InputBox from '../components/InputBox'
 
 function HostScreen() {
-    return (
-        <Text>Configure this device as a host.</Text>
-    )
+	return (
+		<View>
+			<Banner text={'Please provide a host code.'} />
+			<InputBox text="Host code" placeholder="Provide host code here..." />
+		</View>
+	)
 }
 
 export default HostScreen
