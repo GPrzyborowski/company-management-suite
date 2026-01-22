5dd504b37195760e7a69fcf8e2d401f55a1ccc7d
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-22 12:33:25 +0100

    create login code generation function

---
+++ backend/utils/generateLoginCode.js
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt'
+
+const BYTES = 5
+
+async function generateLoginCode() {
+    const code = crypto.randomBytes(BYTES).toString('hex')
+    const hash = await bcrypt.hash(code, 10)
+    return {code, hash}
+}
+
+export default generateLoginCode
