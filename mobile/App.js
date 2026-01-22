7c93cde50d6f2d16a94a0a42188f5702386902a2
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-22 11:20:47 +0100

    add stack navigator animation

--- mobile/App.js
+++ mobile/App.js
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator()
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home">
+			<Stack.Navigator initialRouteName="Home" screenOptions={{ animation: 'slide_from_right' }}>
 				<Stack.Screen name="Home" component={HomeScreen} />
 				<Stack.Screen name="Login" component={LoginScreen} />
 				<Stack.Screen name="Host" component={HostScreen} />
