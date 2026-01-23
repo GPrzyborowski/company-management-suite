c42a06963f962fa96706614f961ddf0c9fca08df
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-23 16:56:23 +0100

    implement login by code function and input box value handling

--- mobile/src/screens/LoginScreen.js
+++ mobile/src/screens/LoginScreen.js
@@ -1,14 +1,42 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native'
+import { useState } from 'react'
 import Banner from '../components/Banner'
 import InputBox from '../components/InputBox'
+import { useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 function LoginScreen() {
+	const [code, setCode] = useState('')
+	const navigation = useNavigation()
+	const localEndpoint = 'http://192.168.133.243:5000/api/auth/loginmobile'
+
+	const handleLogin = async () => {
+		try {
+			const res = await fetch(localEndpoint, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ code }),
+			})
+			if (!res.ok) {
+				throw new Error('Invalid login code.')
+			}
+			const data = await res.json()
+			await AsyncStorage.setItem('token', data.token)
+			navigation.reset({
+				index: 0,
+				routes: [{ name: 'EmployeeDashboard' }],
+			})
+		} catch (err) {
+			Alert.alert('Login was not successful.')
+		}
+	}
+
 	return (
 		<View>
 			<Banner text={'Please provide a login code.'} />
 			<Text style={styles.info}>If you don't have a login code yet, contact your employer.</Text>
-			<InputBox text="Login code" placeholder="Enter your login code..." />
-			<TouchableOpacity style={styles.btn}>
+			<InputBox text="Login code" placeholder="Enter your login code..." value={code} onChangeText={setCode} />
+			<TouchableOpacity style={styles.btn} onPress={handleLogin}>
 				<Text style={styles.btnText}>Log in</Text>
 			</TouchableOpacity>
 		</View>
@@ -34,12 +62,12 @@ const styles = StyleSheet.create({
 	btnText: {
 		color: '#FFF',
 	},
-    info: {
-        marginTop: 16,
-        textAlign: 'center',
-        fontSize: 12,
-        fontStyle: 'italic'
-    }
+	info: {
+		marginTop: 16,
+		textAlign: 'center',
+		fontSize: 12,
+		fontStyle: 'italic',
+	},
 })
 
 export default LoginScreen
