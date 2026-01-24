58603580df175e80dd691a929de59a7e1c07df41
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-24 12:11:39 +0100

    add delete route

--- backend/src/routes/employees.routes.js
+++ backend/src/routes/employees.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import auth from '../middleware/auth.js'
-import { getEmployees, getEmployeeById, updateEmployee } from '../../controllers/employees.controller.js'
+import { getEmployees, getEmployeeById, updateEmployee, removeEmployee } from '../../controllers/employees.controller.js'
 
 const router = express.Router()
 
 router.get('/', auth, getEmployees)
 router.get('/:id', auth, getEmployeeById)
 router.put('/:id', auth, updateEmployee)
+router.delete('/:id', auth, removeEmployee)
 
 export default router
