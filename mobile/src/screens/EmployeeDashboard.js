6d6f31c71141f4cfa1caa0cd4cc79248b330459c
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-01-25 08:32:02 +0100

    display employee name in welcome message

--- mobile/src/screens/EmployeeDashboard.js
+++ mobile/src/screens/EmployeeDashboard.js
@@ -1,11 +1,34 @@
-import { View, Text } from "react-native"
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { View, Text } from 'react-native'
+import { useEffect, useState } from 'react'
 
 function EmployeeDashboard() {
-    return (
-        <View>
-            <Text>Hello world!</Text>
-        </View>
-    )
+
+	const [employee, setEmployee] = useState(null)
+
+    useEffect(() => {
+        const loadEmployee = async () => {
+            const storedData = await AsyncStorage.getItem('employee')
+            if(storedData) {
+                setEmployee(JSON.parse(storedData))
+            }
+        }
+        loadEmployee()
+    }, [])
+
+    if(!employee) {
+        return (
+            <View>
+                <Text>Loading data...</Text>
+            </View>
+        )
+    }
+
+	return (
+		<View>
+			<Text>Hello {employee.firstName}</Text>
+		</View>
+	)
 }
 
 export default EmployeeDashboard
