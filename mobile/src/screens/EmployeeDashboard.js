ddafc48207447de6f42f959594f7db5f64aecf2b
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-01 19:28:54 +0100

    change header spacing

--- mobile/src/screens/EmployeeDashboard.js
+++ mobile/src/screens/EmployeeDashboard.js
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { useEffect, useState } from 'react'
 
 function EmployeeDashboard() {
@@ -25,10 +25,18 @@ function EmployeeDashboard() {
     }
 
 	return (
-		<View>
-			<Text>Hello {employee.firstName}</Text>
+		<View style={styles.container}>
+			<Text style={styles.header}>Hello {employee.firstName}</Text>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+    header: {
+        marginTop: 64,
+        textAlign: 'center',
+        fontSize: 24
+    },
+})
 
 export default EmployeeDashboard
