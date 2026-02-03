8788ecec00f8805db9303a12e7bf62f1cca09a8a
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-03 05:31:29 +0100

    create work status component

---
+++ mobile/src/components/WorkStatus.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+function WorkStatus({ status }) {
+	return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default WorkStatus
