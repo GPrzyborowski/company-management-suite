34965a972c194ec8a1dadb4800fa7d1183ec43aa
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-03 05:41:53 +0100

    change status text spacing

--- mobile/src/components/WorkStatus.js
+++ mobile/src/components/WorkStatus.js
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 function WorkStatus({ isWorking }) {
 
 	return (
         <View>
-            <Text>{isWorking ? 'Active' : 'Not active'}</Text>
+            <Text style={styles.status}>{isWorking ? 'Active' : 'Not active'}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    status: {
+        marginTop: 24,
+        textAlign: 'center'
+    }
+})
 
 export default WorkStatus
