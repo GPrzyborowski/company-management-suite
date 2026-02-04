a89188b7098f2dc65e688d19bf6918b6b2277d60
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-04 01:41:32 +0100

    add status dot

--- mobile/src/components/WorkStatus.js
+++ mobile/src/components/WorkStatus.js
@@ -1,19 +1,41 @@
 import { View, Text, StyleSheet } from 'react-native'
 
 function WorkStatus({ isWorking }) {
-
 	return (
-        <View>
-            <Text style={styles.status}>{isWorking ? 'Active' : 'Not active'}</Text>
-        </View>
-    )
+		<View>
+			<Text style={styles.status}>{isWorking ? 'Active' : 'Not active'}</Text>
+
+			<View style={styles.dotWrapper}>
+				<View style={[styles.statusDot, isWorking ? styles.active : styles.notActive]} />
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    status: {
-        marginTop: 24,
-        textAlign: 'center'
-    }
+	status: {
+		marginTop: 24,
+		textAlign: 'center',
+	},
+
+	dotWrapper: {
+		alignItems: 'center',
+		marginTop: 15,
+	},
+
+	statusDot: {
+		width: 20,
+		height: 20,
+		borderRadius: '50%',
+	},
+
+	active: {
+		backgroundColor: 'green',
+	},
+
+	notActive: {
+		backgroundColor: 'red',
+	},
 })
 
 export default WorkStatus
