ef6427277a4af8b7052d124d9d40c6a90e94a67e
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-05 21:05:19 +0100

    remove unused imports

--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 function HomeScreen({ navigation }) {
 	return (
