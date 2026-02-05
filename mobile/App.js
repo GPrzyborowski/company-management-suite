5aa2c6c6a9a8ce1ed1fa06ed04f8089656a7974b
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-05 21:04:25 +0100

    remove unused imports

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 import HomeScreen from './src/screens/HomeScreen'
 import HostScreen from './src/screens/HostScreen'
 import LoginScreen from './src/screens/LoginScreen'
