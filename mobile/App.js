038d5607124ad613698bffb20ff0078c7cb18c57
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-06 20:58:16 +0100

    add work statistics screen to stack

--- mobile/App.js
+++ mobile/App.js
@@ -5,6 +5,7 @@ import HostScreen from './src/screens/HostScreen'
 import LoginScreen from './src/screens/LoginScreen'
 import EmployeeDashboard from './src/screens/EmployeeDashboard'
 import QrScannerScreen from './src/screens/QrScannerScreen'
+import WorkStatsScreen from './src/screens/WorkStatsScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -17,6 +18,7 @@ export default function App() {
 				<Stack.Screen name="Host" component={HostScreen} />
 				<Stack.Screen name="EmployeeDashboard" component={EmployeeDashboard} options={{title: 'Employee dashboard'}}/>
 				<Stack.Screen name="QrScanner" component={QrScannerScreen} options={{title: 'Scan QR'}}/>
+				<Stack.Screen name="WorkStats" component={WorkStatsScreen} options={{title: 'Work statistics'}}/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
