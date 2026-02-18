8b196da6ddb906369d053a95d42649c0ec1f74cd
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-18 18:45:15 +0100

    add viewport for management tab

---
+++ frontend/src/components/ManagementViewport.jsx
@@ -0,0 +1,11 @@
+import classes from './ManagementViewport.module.css'
+
+function ManagementViewport() {
+    return (
+        <main className={classes.main}>
+
+        </main>
+    )
+}
+
+export default ManagementViewport
