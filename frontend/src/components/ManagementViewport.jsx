626eac41496b8bcdd9a1fcbf6d837e0c278e0313
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-19 18:00:00 +0100

    create host panel

--- frontend/src/components/ManagementViewport.jsx
+++ frontend/src/components/ManagementViewport.jsx
@@ -1,9 +1,10 @@
+import HostPanel from './HostPanel'
 import classes from './ManagementViewport.module.css'
 
 function ManagementViewport() {
     return (
         <main className={classes.main}>
-
+            <HostPanel />
         </main>
     )
 }
