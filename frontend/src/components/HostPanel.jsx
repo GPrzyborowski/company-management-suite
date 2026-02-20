732286f32689eff308f18340bde7bd6699a9914a
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-20 18:00:00 +0100

    create host panel

--- frontend/src/components/HostPanel.jsx
+++ frontend/src/components/HostPanel.jsx
@@ -1,6 +1,11 @@
+import HostCard from "./HostCard"
+
 function HostPanel() {
     return (
-        <div></div>
+        <div>
+            <h2>Host devices</h2>
+            <HostCard />
+        </div>
     )
 }
 
