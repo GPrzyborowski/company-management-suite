c36762de8b600f149fa2bc5cff8b4e0971103bbc
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-22 05:02:00 +0100

    change panel structure

--- frontend/src/components/HostPanel.jsx
+++ frontend/src/components/HostPanel.jsx
@@ -3,9 +3,11 @@ import classes from './HostPanel.module.css'
 
 function HostPanel() {
     return (
-        <div className={classes.header}>
-            <h2>Host devices</h2>
-            <HostCard />
+        <div className={classes.container}>
+            <h2 className={classes.header}>Host devices</h2>
+            <div className={classes['device-container']}>
+                <HostCard />
+            </div>
         </div>
     )
 }
