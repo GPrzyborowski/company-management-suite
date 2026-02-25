f62ac47dd41f40e32f43546268ad92aec040fd44
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-25 13:41:27 +0100

    add host panel class

--- frontend/src/components/ManagementViewport.jsx
+++ frontend/src/components/ManagementViewport.jsx
@@ -4,7 +4,7 @@ import classes from './ManagementViewport.module.css'
 function ManagementViewport() {
     return (
         <main className={classes.main}>
-            <HostPanel />
+            <HostPanel className={classes.host}/>
         </main>
     )
 }
