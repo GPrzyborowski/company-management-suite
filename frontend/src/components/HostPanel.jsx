648db09eea30353626900c68aeae88a8ba187fa3
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-02-28 16:03:33 +0100

    change structure

--- frontend/src/components/HostPanel.jsx
+++ frontend/src/components/HostPanel.jsx
@@ -1,16 +1,19 @@
-import HostCard from "./HostCard"
-import NewHostCard from "./NewHostCard"
+import HostCard from './HostCard'
+import NewHostCard from './NewHostCard'
 import classes from './HostPanel.module.css'
 
+
 function HostPanel() {
-    return (
-        <div className={classes.container}>
-            <h2 className={classes.header}>Host devices</h2>
-            <div className={classes['device-container']}>
-                <NewHostCard />
-            </div>
-        </div>
-    )
+	return (
+		<>
+			<div className={classes.container}>
+				<h2 className={classes.header}>Host devices</h2>
+				<div className={classes['device-container']}>
+					<NewHostCard />
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default HostPanel
