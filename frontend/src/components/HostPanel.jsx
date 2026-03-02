b0956c8997c34643274a4f3b26feff206f4954de
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-02 21:42:51 +0100

    pass onclick prop

--- frontend/src/components/HostPanel.jsx
+++ frontend/src/components/HostPanel.jsx
@@ -2,18 +2,15 @@ import HostCard from './HostCard'
 import NewHostCard from './NewHostCard'
 import classes from './HostPanel.module.css'
 
-
-function HostPanel() {
-	return (
-		<>
-			<div className={classes.container}>
-				<h2 className={classes.header}>Host devices</h2>
-				<div className={classes['device-container']}>
-					<NewHostCard />
-				</div>
-			</div>
-		</>
-	)
+function HostPanel({ onNewHostClick }) {
+  return (
+    <div className={classes.container}>
+      <h2 className={classes.header}>Host devices</h2>
+      <div className={classes['device-container']}>
+        <NewHostCard onClick={onNewHostClick} />
+      </div>
+    </div>
+  )
 }
 
 export default HostPanel
