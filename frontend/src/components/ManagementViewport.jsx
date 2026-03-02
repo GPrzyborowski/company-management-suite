26a6e2d4e21cad3448d5ca0fc53763cc11e1830c
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-02 21:42:51 +0100

    create modal toggle

--- frontend/src/components/ManagementViewport.jsx
+++ frontend/src/components/ManagementViewport.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import HostPanel from './HostPanel'
+import NewHostModal from './NewHostModal'
 import classes from './ManagementViewport.module.css'
 
 function ManagementViewport() {
-	return (
-		<main className={classes.main}>
-			<HostPanel className={classes.host} />
-		</main>
-	)
+  const [confirmVisible, setConfirmVisible] = useState(false)
+
+  const openModal = () => setConfirmVisible(true)
+  const closeModal = () => setConfirmVisible(false)
+
+  return (
+    <main className={classes.main}>
+      <HostPanel className={classes.host} onNewHostClick={openModal} />
+
+      <NewHostModal
+        confirmVisible={confirmVisible}
+        onCancel={closeModal}
+      />
+    </main>
+  )
 }
 
 export default ManagementViewport
