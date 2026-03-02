1a6248f3bee384461abfa2156074ddf0ac5c772f
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-02 21:42:51 +0100

    add onclick prop

--- frontend/src/components/NewHostCard.jsx
+++ frontend/src/components/NewHostCard.jsx
@@ -1,10 +1,10 @@
 import classes from './NewHostCard.module.css'
 
-function NewHostCard() {
+function NewHostCard({ onClick }) {
 	return (
-		<div className={classes.card}>
+		<div className={classes.card} onClick={onClick}>
 			<p className={classes.name}>New host device</p>
-            <img className={classes.icon} src="/plus-circle.svg" alt="plus circle" />
+			<img className={classes.icon} src="/plus-circle.svg" alt="plus circle" />
 		</div>
 	)
 }
