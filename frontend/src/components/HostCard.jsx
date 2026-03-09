a2cefd79ab14caa642389eb893f129aa16733cbf
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-09 03:42:35 +0100

    display data from props and contitionally render status dot

--- frontend/src/components/HostCard.jsx
+++ frontend/src/components/HostCard.jsx
@@ -1,17 +1,24 @@
 import classes from './HostCard.module.css'
 
-function HostCard() {
+function HostCard({ hostName, createdDate, isActivated }) {
+	const newCreateDate = new Date(createdDate)
+	const createdDateFormatted = newCreateDate.toLocaleDateString('pl-PL', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	})
+
 	return (
 		<div className={classes.card}>
-			<p className={classes.name}>Tablet</p>
+			<p className={classes.name}>{hostName}</p>
 			<div className={classes.container}>
 				<div className={classes['card-activated']}>
 					<img src="/calendar.svg" alt="calendar icon" className={classes['calendar-icon']} />
-					<p className={classes.date}>01.01.2025</p>
+					<p className={classes.date}>{createdDateFormatted}</p>
 				</div>
 				<div className={classes['card-active']}>
-					<div className={classes.dot}></div>
-					<p>Active</p>
+					<div className={`${classes.dot} ${isActivated ? classes['dot-activated'] : ''}`}></div>
+					<p>{isActivated ? 'Active' : 'Not active'}</p>
 				</div>
 				<div className={classes['btn-container']}>
 					<button className={classes.btn}>
