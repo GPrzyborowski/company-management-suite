77106a0a17235c35aec99a659ef6c62f93bdd75a
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-09 03:44:00 +0100

    map hosts as card components

--- frontend/src/components/HostPanel.jsx
+++ frontend/src/components/HostPanel.jsx
@@ -2,15 +2,25 @@ import HostCard from './HostCard'
 import NewHostCard from './NewHostCard'
 import classes from './HostPanel.module.css'
 
-function HostPanel({ onNewHostClick }) {
-  return (
-    <div className={classes.container}>
-      <h2 className={classes.header}>Host devices</h2>
-      <div className={classes['device-container']}>
-        <NewHostCard onClick={onNewHostClick} />
-      </div>
-    </div>
-  )
+function HostPanel({ onNewHostClick, hosts }) {
+	return (
+		<div className={classes.container}>
+			<h2 className={classes.header}>Host devices</h2>
+			<div className={classes['device-container']}>
+				{hosts.map(host => {
+					return (
+						<HostCard
+							key={host.id}
+							hostName={host.deviceName}
+							createdDate={host.createdAt}
+							isActivated={host.isActive}
+						/>
+					)
+				})}
+				<NewHostCard onClick={onNewHostClick} />
+			</div>
+		</div>
+	)
 }
 
 export default HostPanel
