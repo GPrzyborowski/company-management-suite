325574163103a124ccccd3940f91a4025d09689e
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-19 13:47:26 +0100

    implement login key generation modal behavior

--- frontend/src/components/HostPanel.jsx
+++ frontend/src/components/HostPanel.jsx
@@ -1,11 +1,58 @@
 import HostCard from './HostCard'
 import NewHostCard from './NewHostCard'
+import LoginKeyModal from './LoginKeyModal'
 import classes from './HostPanel.module.css'
-import { API_URL } from "../config/env"
+import { useState } from 'react'
+import { API_URL } from '../config/env'
 
 function HostPanel({ onNewHostClick, hosts, fetchHosts }) {
+	const [name, setName] = useState('')
+	const [loginKeyVisible, setLoginKeyVisible] = useState(false)
+	const [loginKey, setLoginKey] = useState('')
+	const [codeVisible, setCodeVisible] = useState(false)
+	const [expiry, setExpiry] = useState(7)
 
 	const TOGGLE_ENDPOINT = `${API_URL}/togglehost`
+	const LOGIN_CODE_ENDPOINT = `${API_URL}/logincode`
+
+	const handleExpiryChange = e => {
+		const value = e.target.value
+		if (value >= 1 && value <= 30) {
+			setExpiry(value)
+		} else if (value == '') {
+			setExpiry('')
+		}
+	}
+
+	const showGenerateLoginKey = () => {
+		setLoginKeyVisible(true)
+	}
+
+	const closeGenerateLoginKey = () => {
+		setCodeVisible(false)
+		setLoginKeyVisible(false)
+	}
+
+	const generateLoginKey = async () => {
+		const res = await fetch(LOGIN_CODE_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+			body: JSON.stringify({
+				employeeId: id,
+				expiresInDays: expiry,
+			}),
+		})
+		if (!res.ok) {
+			console.error(res)
+			return
+		}
+		const data = await res.json()
+		setLoginKey(data.oneTimeCode)
+		setCodeVisible(true)
+	}
 
 	const toggleActivated = async id => {
 		const res = await fetch(`${TOGGLE_ENDPOINT}/${id}`, {
@@ -22,8 +69,22 @@ function HostPanel({ onNewHostClick, hosts, fetchHosts }) {
 		fetchHosts()
 	}
 
+	const toggleKeyModal = async () => {
+		showGenerateLoginKey()
+	}
+
 	return (
 		<div className={classes.container}>
+			<LoginKeyModal
+				name={name}
+				visible={loginKeyVisible}
+				expiry={expiry}
+				onChangeExpiry={handleExpiryChange}
+				onGenerate={generateLoginKey}
+				onClose={closeGenerateLoginKey}
+				codeVisible={codeVisible}
+				code={loginKey}
+			/>
 			<h2 className={classes.header}>Host devices</h2>
 			<div className={classes['device-container']}>
 				{hosts.map(host => {
@@ -34,6 +95,10 @@ function HostPanel({ onNewHostClick, hosts, fetchHosts }) {
 							createdDate={host.createdAt}
 							isActivated={host.isActive}
 							onPowerClick={() => toggleActivated(host.id)}
+							onKeyClick={() => {
+								setName(host.deviceName)
+								toggleKeyModal()
+							}}
 						/>
 					)
 				})}
