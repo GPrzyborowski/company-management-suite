3218a550e79b815884771052506ea4c21c524bcb
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-22 15:08:17 +0100

    add device login code generation endpoint

--- backend/src/routes/logincode.routes.js
+++ backend/src/routes/logincode.routes.js
@@ -33,4 +33,31 @@ router.post('/logincode', async (req, res) => {
     })
 })
 
+router.post('/devicelogincode', async (req, res) => {
+	const { deviceId, expiresInDays } = req.body
+    const deviceIdNumber = Number(deviceId)
+    const days = Number(expiresInDays)
+    if(!days || days <= 0 || days > 30) {
+        return res.status(400).json({message: 'Invalid expiration time.'})
+    }
+    const device = await prisma.employee.findUnique({where: {id: deviceIdNumber}})
+    if(!employee) {
+        return res.status(404).json({message: "Device does not exist."})
+    }
+    const {code, hash} = await generateCode()
+    const MS_IN_DAY = 1000 * 60 * 60 * 24
+    const expiresAt = new Date(Date.now() + days * MS_IN_DAY)
+    await prisma.deviceLoginCode.create({
+        data: {
+            codeHash: hash,
+            expiresAt,
+            deviceId: deviceIdNumber
+        }
+    })
+    res.json({
+        oneTimeCode: code,
+        expiresAt
+    })
+})
+
 export default router
