2d15a162fc110557c1e75f5b7f9737970f8630ec
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-26 21:44:01 +0100

    add health endpoint and import device routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth.routes.js'
 import employeeDocumentsRoutes from './routes/employeedocs.routes.js'
 import loginCode from './routes/logincode.routes.js'
 import hosts from './routes/hosts.routes.js'
+import device from './routes/device.routes.js'
 
 const app = express()
 
@@ -17,5 +18,10 @@ app.use('/api/employees', employeesRoutes)
 app.use('/api', employeeDocumentsRoutes)
 app.use('/api', loginCode)
 app.use('/api/hosts', hosts)
+app.use('/api/device', device)
+
+app.get('/health', (req, res) => {
+    res.json({message: "Server OK"})
+})
 
 app.listen(5000, () => console.log("Server running"))
