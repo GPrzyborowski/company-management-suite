4d593ac27a365f88ca5e662c6e85db339bcdc7fd
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-26 21:42:17 +0100

    add endpoint to generate qr code data

---
+++ backend/src/routes/device.routes.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import prisma from '../config/prisma.js'
+import crypto from 'crypto'
+
+const router = express.Router()
+
+router.post('/generateQr', async (req, res) => {
+	const { deviceId, type } = req.body
+	const token = crypto.randomBytes(32).toString('hex')
+	const expiresAt = new Date(Date.now() + 60 * 1000)
+
+	const code = await prisma.deviceLoginCode.create({
+		data: {
+			deviceId,
+			codeHash: token,
+			expiresAt,
+		},
+	})
+
+	res.json({
+		qrData: {
+			type,
+			deviceId,
+			token,
+		},
+	})
+})
+
+export default router
