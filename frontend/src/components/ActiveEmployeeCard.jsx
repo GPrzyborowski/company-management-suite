4f6497ad8a8e91d422e2abb2c577fc3f9e7020ad
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-29 23:49:56 +0200

    create card structure

---
+++ frontend/src/components/ActiveEmployeeCard.jsx
@@ -0,0 +1,11 @@
+function ActiveEmployeeCard({name, surname, workStartTime}) {
+    return (
+        <div>
+            <div></div>
+            <p>{name} {surname}</p>
+            <p>{workStartTime}</p>
+        </div>
+    )
+}
+
+export default ActiveEmployeeCard
