a09397f2c26e49a169aa97cfeb8f05ad42c0392b
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-19 13:52:18 +0200

    verify if user exists based on provided token

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,19 +1,28 @@
 import jwt from 'jsonwebtoken'
+import prisma from '../config/prisma.js'
 
-const auth = (req, res, next) => {
-    const header = req.headers.authorization
-    if(!header) {
-        return res.status(401).json({error: "Token not provided."})
-    }
-    const token = header.split(" ")[1]
+const auth = async (req, res, next) => {
+	const header = req.headers.authorization
+	if (!header) {
+		return res.status(401).json({ error: 'Token not provided.' })
+	}
+	const token = header.split(' ')[1]
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = decoded
-        next()
-    } catch(err) {
-        return res.status(401).json({error: "Invalid token."})
-    }
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET)
+		const user = await prisma.admin.findUnique({
+			where: {
+				id: decoded.id,
+			},
+		})
+		if (!user) {
+			return res.status(401).json({ error: 'User does not exist.' })
+		}
+		req.user = user
+		next()
+	} catch (err) {
+		return res.status(401).json({ error: 'Invalid token.' })
+	}
 }
 
 export default auth
