8a7526ac74f1a8e8e78bd8bbd17a0172257de966
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-20 21:09:36 +0200

    add active employees section

--- frontend/src/components/DashboardMain.jsx
+++ frontend/src/components/DashboardMain.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
 import { jwtDecode } from 'jwt-decode'
 import classes from './DashboardMain.module.css'
+import ActiveEmployeeCard from './ActiveEmployeeCard'
 
 function DashboardMain() {
 	const [login, setLogin] = useState('')
+	const [activeEmployees, setActiveEmployees] = useState([])
 
 	useEffect(() => {
 		const token = localStorage.getItem('token')
@@ -18,20 +20,22 @@ function DashboardMain() {
 		}
 	}, [])
 
-    const getGreeting = () => {
-        const hour = new Date().getHours()
-        if((hour > 4) && (hour < 12)) {
-            return `Good morning ${login}.`
-        } else if((hour > 12) && (hour < 19)) {
-            return `Good afternoon ${login}.`
-        } else {
-            return `Good evening ${login}.`
-        }
-    }
+	const getGreeting = () => {
+		const hour = new Date().getHours()
+		if (hour > 4 && hour < 12) {
+			return `Good morning ${login}.`
+		} else if (hour > 12 && hour < 19) {
+			return `Good afternoon ${login}.`
+		} else {
+			return `Good evening ${login}.`
+		}
+	}
 
 	return (
 		<main className={classes['dashboard-container']}>
 			<h1 className={classes['main-header']}>{getGreeting()}</h1>
+			<h2 className={`${classes['active-employees-header']} ${activeEmployees.length == 0 ? classes.hidden : ''}`}>Currently at work</h2>
+			<div className={classes['active-employees-container']}></div>
 		</main>
 	)
 }
