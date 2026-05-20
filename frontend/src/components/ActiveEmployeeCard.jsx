6033c6b04446bee8c8102feed940d185abb02ede
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-20 21:09:01 +0200

    create card structure

--- frontend/src/components/ActiveEmployeeCard.jsx
+++ frontend/src/components/ActiveEmployeeCard.jsx
@@ -1,16 +1,19 @@
 import classes from './ActiveEmployeeCard.module.css'
 
-function ActiveEmployeeCard({name, surname, workStartTime}) {
-    return (
-        <div className={classes.card}>
-            <div className={classes.dot}></div>
-            <div>
-                <img src="user icon" alt="/user.svg" />
-                <p className={classes.name}>{name} {surname}</p>
-            </div>
-            <p className={classes.time}>Clock-in: {workStartTime}</p>
-        </div>
-    )
+function ActiveEmployeeCard({ name, surname, workStartTime }) {
+	return (
+		<div className={classes.card}>
+			<div className={classes['activity-container']}>
+				<div className={classes.dot}></div>
+                <p>Working</p>
+			</div>
+			<p className={classes.name}>
+				{name} {surname}
+			</p>
+
+			<p className={classes.time}>Clock-in: {workStartTime}</p>
+		</div>
+	)
 }
 
 export default ActiveEmployeeCard
